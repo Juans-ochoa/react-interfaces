faca57b07c66b0efdd0bee016f717dcbbec1b7b4
Sebastian Ochoa <sebastian.ochoa.dev@gmail.com>
2022-06-12 14:27:50 -0500

    Borrado de carpeta Accordion

--- src/components/Accordion/Accordion.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./styles.css";
-
-const Accordion = ({ children }) => {
-  return <section className="accordion__container">{children}</section>;
-};
-
-export default Accordion;
